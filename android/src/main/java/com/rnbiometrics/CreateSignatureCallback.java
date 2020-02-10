234a485a1d90556b5388e62c48f573d490001b3b
Xuemin Liu <modenl@gmail.com>
2020-02-10 13:49:28 -0800

    remove dup

--- android/src/main/java/com/rnbiometrics/CreateSignatureCallback.java
+++ android/src/main/java/com/rnbiometrics/CreateSignatureCallback.java
@@ -24,7 +24,6 @@ public class CreateSignatureCallback extends BiometricPrompt.AuthenticationCallb
     @Override
     public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
         super.onAuthenticationError(errorCode, errString);
-        super.onAuthenticationError(errorCode, errString);
         if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON || errorCode == BiometricPrompt.ERROR_USER_CANCELED ) {
             WritableMap resultMap = new WritableNativeMap();
             resultMap.putBoolean("success", false);
